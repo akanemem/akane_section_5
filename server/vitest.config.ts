2f2ff71912048cc92b7599ce52d65d5ebe053831
akanemem <minami.pig.cat.dog.peekaboo@gmail.com>
2025-11-13 21:53:28 +0900

    npm run test でテストを実行できるようにvitestをクライアントとサーバーにインストール

---
+++ server/vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',   // ← バックエンドなので node 環境
+    include: ['src/**/*.test.ts'], // ← テストファイルを src 配下に限定
+  }
+});
